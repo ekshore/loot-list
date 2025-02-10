46f4136ef0f441f9cf97055f9b562b9bcac137cc
Caleb Ekstrand <ekstrand.caleb@ekshore.net>
2025-02-10 12:39:58 -0600

    Server functions
    Utilizing server functions to get data from the DB.
    Also hiding creating a new list behind auth.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,39 +10,29 @@ import {
 } from "~/components/ui/card";
 
 import { auth } from "~/server/auth";
-import { api, HydrateClient } from "~/trpc/server";
+import { db } from "~/server/db";
+import { lists } from "~/server/db/schema";
+import { desc } from "drizzle-orm";
 
 export default async function Home() {
-  const session = await auth();
+  return (
+    <main className="container m-auto min-h-screen border-x border-x-border">
+      <div className="flex flex-col items-center">
+        <h1 className="py-5 text-5xl font-bold">Ekstrand Family Lists</h1>
+        <ListDisplay />
+      </div>
+    </main>
+  );
+}
 
-  const lists = [
-    {
-      id: "someId-1",
-      userId: "someUserId",
-      name: "Test List",
-      summary: "This is a list for testing",
-      createdAt: "2025-02-09 22:45:26.191893+00",
-      updatedAt: "2025-02-09 22:45:26.191893+00",
-    },
-    {
-      id: "someId-2",
-      userId: "someUserId",
-      name: "Test List",
-      summary: "This is a list for testing",
-      createdAt: "2025-02-09 22:45:26.191893+00",
-      updatedAt: "2025-02-09 22:45:26.191893+00",
-    },
-    {
-      id: "someId-3",
-      userId: "someUserId",
-      name: "Test List",
-      summary: "This is a list for testing",
-      createdAt: "2025-02-09 22:45:26.191893+00",
-      updatedAt: "2025-02-09 22:45:26.191893+00",
-    },
-  ];
+const ListDisplay = async () => {
+  "server";
+  const wishLists = await db
+    .select()
+    .from(lists)
+    .orderBy(desc(lists.updatedAt));
 
-  const listCards = lists.map((list) => {
+  const listCards = wishLists.map((list) => {
     const listUrl = `/mylists/${list.id}`;
     return (
       <Link key={list.id} href={listUrl} className="m-5">
@@ -56,7 +46,7 @@ export default async function Home() {
           <CardFooter className="basis-1/4">
             Last updated:&nbsp;
             <span className="text-xs text-muted-foreground">
-              {formatDate(list.createdAt)}
+              {list.createdAt.toLocaleDateString()}
             </span>
           </CardFooter>
         </Card>
@@ -64,30 +54,18 @@ export default async function Home() {
     );
   });
 
-  listCards.push(
-    <Link key="create-list" href="/" className="m-5">
-      <Card className="flex items-center justify-center h-60 w-72">
-        <CardContent className="p-0 flex items-center justify-center">
-          <SquarePlus className="size-20 text-border"/>
-        </CardContent>
-      </Card>
-    </Link>,
-  );
+  const session = await auth();
+  if (session?.user) {
+    listCards.push(
+      <Link key="create-list" href="/" className="m-5">
+        <Card className="flex h-60 w-72 items-center justify-center">
+          <CardContent className="flex items-center justify-center p-0">
+            <SquarePlus className="size-20 text-border" />
+          </CardContent>
+        </Card>
+      </Link>,
+    );
+  }
 
-  return (
-    <HydrateClient>
-      <main className="container m-auto min-h-screen border-x border-x-border">
-        <div className="flex flex-col items-center">
-          <h1 className="py-5 text-5xl font-bold">Ekstrand Family Lists</h1>
-          <div className="grid grid-cols-3">{listCards}</div>
-        </div>
-      </main>
-    </HydrateClient>
-  );
-}
-
-const formatDate = (ts: String) => {
-  const date = ts.substring(0, 10);
-  const [year, month, day] = date.split("-");
-  return `${month}/${day}/${year}`;
+  return <div className="grid grid-cols-3">{listCards}</div>;
 };
